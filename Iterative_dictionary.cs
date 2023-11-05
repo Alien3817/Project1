a077b9378acd335922e9313f02d50c6a7f8a55df
Alien3817 <133295602+Alien3817@users.noreply.github.com>
2023-11-05 21:03:32 +1000

    остальные файлы к переделанному фронту

---
+++ Iterative_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Front
+{
+    internal class Iterative_dictionary
+    {
+        public static Dictionary<string, int> It_dic;
+    }
+}
