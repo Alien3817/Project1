892788d8ecd1eed76e7fea770bca98c71b5451b6
Strory <hstrory@mail.ru>
2023-11-12 20:58:17 +1000

    Languages

--- Front/Form1.Designer.cs
+++ Front/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,9 +52,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Image = global::Front.Properties.Resources.Menu;
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(58, 44);
+            this.button1.Size = new System.Drawing.Size(77, 54);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -64,9 +66,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(109, 97);
+            this.button2.Location = new System.Drawing.Point(145, 119);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(747, 100);
+            this.button2.Size = new System.Drawing.Size(996, 123);
             this.button2.TabIndex = 5;
             this.button2.Text = "Введите ваш текст...";
             this.button2.UseVisualStyleBackColor = false;
@@ -76,33 +79,35 @@
             // 
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(109, 256);
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(145, 315);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(766, 244);
+            this.listBox1.Size = new System.Drawing.Size(1020, 279);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.оПрограммеToolStripMenuItem,
             this.командаПректаToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(163, 48);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(192, 52);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // оПрограммеToolStripMenuItem
             // 
             this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
-            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
             this.оПрограммеToolStripMenuItem.Text = "О программе";
             this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
             // 
             // командаПректаToolStripMenuItem
             // 
             this.командаПректаToolStripMenuItem.Name = "командаПректаToolStripMenuItem";
-            this.командаПректаToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.командаПректаToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
             this.командаПректаToolStripMenuItem.Text = "Команда пректа";
             this.командаПректаToolStripMenuItem.Click += new System.EventHandler(this.командаПректаToolStripMenuItem_Click);
             // 
@@ -110,9 +115,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(148, 217);
+            this.label1.Location = new System.Drawing.Point(197, 267);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.Size = new System.Drawing.Size(99, 25);
             this.label1.TabIndex = 7;
             this.label1.Text = "Алфавит";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -121,9 +127,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(293, 217);
+            this.label2.Location = new System.Drawing.Point(391, 267);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.Size = new System.Drawing.Size(146, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "Двоичный код";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -131,9 +138,10 @@
             // button3
             // 
             this.button3.Image = global::Front.Properties.Resources.кнопка_поиска;
-            this.button3.Location = new System.Drawing.Point(809, 212);
+            this.button3.Location = new System.Drawing.Point(1079, 261);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(66, 32);
+            this.button3.Size = new System.Drawing.Size(88, 39);
             this.button3.TabIndex = 9;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -141,44 +149,58 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(603, 212);
-            this.textBox1.MaximumSize = new System.Drawing.Size(200, 32);
-            this.textBox1.MinimumSize = new System.Drawing.Size(200, 32);
+            this.textBox1.Location = new System.Drawing.Point(804, 261);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.MaximumSize = new System.Drawing.Size(265, 32);
+            this.textBox1.MinimumSize = new System.Drawing.Size(265, 32);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 32);
+            this.textBox1.Size = new System.Drawing.Size(265, 34);
             this.textBox1.TabIndex = 10;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(109, 97);
-            this.textBox2.MaximumSize = new System.Drawing.Size(766, 100);
-            this.textBox2.MinimumSize = new System.Drawing.Size(766, 100);
+            this.textBox2.Location = new System.Drawing.Point(145, 119);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.MaximumSize = new System.Drawing.Size(1020, 122);
+            this.textBox2.MinimumSize = new System.Drawing.Size(1020, 122);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(766, 100);
+            this.textBox2.Size = new System.Drawing.Size(1020, 122);
             this.textBox2.TabIndex = 11;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button4
             // 
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(826, 49);
+            this.button4.Location = new System.Drawing.Point(1101, 60);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(49, 42);
+            this.button4.Size = new System.Drawing.Size(65, 52);
             this.button4.TabIndex = 12;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Visible = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(158, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(639, 93);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "сменить язык на английски и обратно (УДАЛИТЬ)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(1309, 681);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
@@ -189,9 +211,10 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1000, 600);
-            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.MaximumSize = new System.Drawing.Size(1327, 728);
+            this.MinimumSize = new System.Drawing.Size(1327, 728);
             this.Name = "Form1";
             this.Text = "Shamdivgla";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -215,6 +238,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
